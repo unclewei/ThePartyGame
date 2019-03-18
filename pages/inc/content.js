606316f6341c7b38c6e06172277d269ea06ac3d3
uncle_wei <784847001@qq.com>
2019-03-18 23:27:31 +0800

    the news

--- pages/inc/content.js
+++ pages/inc/content.js
@@ -17,7 +17,20 @@ const imgs = [
   '../images/img16.jpg',
   '../images/img17.jpg',
   '../images/img18.jpg',
-  '../images/img19.jpg'
+  '../images/img19.jpg',
+  '../images/img20.jpg',
+  '../images/img21.jpg',
+  '../images/img22.jpg',
+  '../images/img23.jpg',
+  '../images/img24.jpg',
+  '../images/img25.jpg',
+  '../images/img26.jpg',
+  '../images/img27.jpg',
+  '../images/img28.jpg',
+  '../images/img29.jpg', 
+  '../images/img30.jpg',
+  '../images/img31.jpg',
+  '../images/img32.jpg',
 ];
 
 const contents = [{
@@ -308,6 +321,497 @@ const contents = [{
   {
     title: 'The Truth',
     text: '你觉得你是渣男/女吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你有约过炮吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说一说你对约炮这件事的看法',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '灵魂出轨和肉体出轨哪个更不能接受？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你是个害怕孤独的人吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你希望有一个生活上不打扰的长期炮友吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说说你现在最想要得到什么？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '坚持做过最久的事情是什么，多久？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '有写日记的习惯吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '对于感情，你是主动的一个还是被动的一个？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '相信一见钟情吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: 'XXOO的时候，回说dirty word吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说一个自己最喜欢的AV女忧\男性幻想对象',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '能接受另一半是外国人吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你是一个恋旧的人吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '理想的职业？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '童年时候的梦想是什么？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你觉得，人活着，是为了什么？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你是一个时常思考人生的人吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你享受性吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '五百万和清华大学的录取通知书你选择哪个？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '月薪5万一个月和1000万，你选择哪个？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说一个你的小嗜好',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说一部你最喜欢的电影',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说一下，你上一次看的书，是什么书',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你能接受多少天不洗澡？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '最喜欢的一个综艺节目',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '能接受情趣内衣吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说你最喜欢的歌手',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '上一次哭，是什么时候，为什么',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '给你1个15天的假期，你会做什么？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '你认为人最重要的品质是什么',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '课余/下班，后会做什么',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '有试过野外XXOO吗？(不提议哟)',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '试过车震吗？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '会不会在某一个深夜想起初恋，并想知道他/她在做什么，和谁一起，过的怎么样？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '用一个词形容你的初恋，和用一个词形容你们在一起的时光',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '喜欢聚会还是喜欢自己一个人？',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '说说，除了微信，用的最多的三个手机APP',
+    contentType: 1
+  },
+  {
+    title: 'The Truth',
+    text: '喜欢一帆风顺的人生，还是翻云覆海的人生？',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '秀一下你的内裤',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '(环境允许的情况下)和左边第一个人，做情侣俯卧撑，你在上，他/她在下。',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '亲一下你左边第二个人的脸颊',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '与右边第一个人，鼻子碰鼻子，深情凝望十秒钟',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '脱下一件衣服',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '和左边第一位异性交换一件衣服穿',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '涂上口红，亲一下你右边第三个人的脸颊，不许擦除',
+    contentType: 1
+  },
+  {
+    title: 'The Dare ',
+    text: '双手捧着左边第一个说一声，“我爱你”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '发一条朋友圈，“有约炮的吗？私聊！”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '做一个长腿撩腿的骚气动作',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '呻吟5秒（不合格的喝酒）',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '拍一下左边第一个的屁股',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '发一条朋友圈，“拒绝就拒绝嘛，何必泼我一脸水，哎”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '左边开始的第一个男生抱着你，做3个深蹲',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '做一个你认为最骚的动作',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '答应你左边第一个人的一个请求，24小时有效',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '收藏这个小程序到我的小程序(作者偷笑哈哈哈哈哈哈)',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '别看了，就是你，喝一杯',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '和左边第一位十指相扣，三轮后才允许松开',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '被一个人用口红在脸上画一笔',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '男生对左边第一个女生高喊：“娘娘吉祥，千岁千岁千千岁”\n女生对右边第一个男生温柔的说：“亲爱的，你就从了我吧~”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '游戏结束后，和父母发微信：“做你们的儿子/女儿真好，我会加倍努力，让你们开心”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '拍一张当场的异性的照片到朋友圈，不配任何文字',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '对你左边第一个人说一句你最想对她/他说的话',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '听你左边的第一个人说一句，他/她想对你说的话',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '尾指被涂指甲油',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '大拇指被涂指甲油',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '对左边第一个异性问：“你最喜欢的做爱姿势是什么？”，需要回答',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '模仿青楼拉客',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '说一个你最邪恶的想法',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '模仿张学友表情包，“压屎啊你！”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '模仿姚明尬笑的表情包，不合格喝酒',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '装傻子笑',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '模仿猩猩',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '壁咚右边第一个异性，说；“你觉得我，怎么样？”',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '摆三个健美先生的姿势',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '讲一个荤段子，讲不出喝酒',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '做一个楚楚可怜的表情',
+    contentType: 1
+  },
+
+  {
+    title: 'The Dare',
+    text: '一句话评价再坐的各位',
+    contentType: 1
+  },
+  {
+    title: 'The Dare',
+    text: '秀出你的腹肌，小肚腩，大肚腩',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '在坐的还是处男，处女的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '认为自己最近过的很糟糕的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '有处女/男情结的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '认为自己最近过的很糟糕的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '觉得自己是和闷骚的人，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '觉得自己是寂寞的人，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: 'm或者有m倾向的的干一杯，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: 's或者有s倾向的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: 's或者有s倾向的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '认为自己老了的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '认为自己还年轻的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '喜欢这个游戏的，干一杯 (作者偷笑脸)',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '觉得自己是变态的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '最求异性第一看脸的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '最求异性第一看身材的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '最求异性第一看性格的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '相信一见钟情的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '用苹果手机的，干一杯',
+    contentType: 1
+  },
+  {
+    title: '刮风',
+    text: '用Android手机的，干一杯',
     contentType: 1
   },
 ];
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,9 @@
 //获取应用实例
 const app = getApp()
 var setCardTime = 800;
+var localContentList = [];
+var randomImgsNub = ''
+var randomContentNub = ''
 import {
   contents,
   imgs
@@ -46,12 +49,16 @@ Page({
    */
   randomCard: function() {
     var that = this;
-    var randomImgsNub = that.random(0, imgs.length);
-    var randomContentNub = that.random(0, contents.length);
+    if (localContentList.length == 0){
+      localContentList = contents;
+    }
+    randomImgsNub = that.random(0, imgs.length);
+    randomContentNub = that.random(0, localContentList.length);
     that.setData({
       image: imgs[randomImgsNub],
-      content: contents[randomContentNub]
+      content: localContentList[randomContentNub]
     })
+    localContentList.splice(randomContentNub,1);
   },
 
   /**
@@ -65,22 +72,43 @@ Page({
     return num;
   },
 
-  onLoad: function() {
+  onLoad: function(e) {
+    console.log(e)
+    localContentList = contents;
+    this.showDataBegain(e);
     var phoneInfo = wx.getSystemInfoSync();
     if (phoneInfo.system.indexOf('iOS') != -1 || phoneInfo.model.indexOf('iPhone') != -1){
       setCardTime = 750;
       console.log('是苹果系统')
     }
   },
+  gotoExplain(){
+    wx.navigateTo({
+      url: '../decode/decode',
+    })
+  },
 
+  showDataBegain(options){
+    var that = this
+    var randomContentNub = options.randomContentNub;
+    var randomImgsNub = options.randomImgsNub;
+    if (randomImgsNub || randomContentNub){
+      that.setData({
+        image: imgs[randomImgsNub],
+        content: localContentList[randomContentNub]
+      })
+      localContentList.splice(randomContentNub, 1);
+    }
+  },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function() {
     return {
-      title: '小程序',
-      path: '/pages/index/index'
+      title: `【${this.data.content.title}】${this.data.content.text}`,
+      path: `/pages/index/index?randomContentNub=${randomContentNub}&randomImgsNub==${randomImgsNub}`,
+      imageUrl: this.data.image,
     }
   }
 
